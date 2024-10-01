59f8b51db24ea98a388162bac84f5d8175ff213d
beebucketjosh <jl@beebucket.de>
2024-10-01 12:50:40 +0200

    Change columns to be key value pairs

--- edp/analyzers/pandas.py
+++ edp/analyzers/pandas.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 from logging import getLogger
-from typing import AsyncIterator, List, Sequence
+from typing import AsyncIterator, Tuple
 
 from numpy import max as numpy_max
 from numpy import mean, median
@@ -26,55 +26,44 @@ class Pandas(Analyzer):
     def __init__(self, data: DataFrame):
         self._logger = getLogger(__name__)
         self._data = data
-        self._column_analyzers = {
-            NumericColumn: self._analyze_numeric_column,
-            DateTimeColumn: self._analyze_datetime_column,
-            StringColumn: self._analyze_string_column,
-        }
 
     @property
     def data_set_type(self):
         return DataSetType.structured
 
     async def analyze(self) -> StructuredEDPDataSet:
-        columns = [column async for column in self._analyze_columns()]
+        columns = {name: column async for name, column in self._analyze_columns()}
         return StructuredEDPDataSet(rowCount=len(self._data.index), columns=columns)
 
-    async def _analyze_columns(self) -> AsyncIterator[Column]:
+    async def _analyze_columns(self) -> AsyncIterator[Tuple[str, Column]]:
         for column_name in self._data.columns:
-            column_type = self._translate_type(column_name)
-            if column_type is NumericColumn:
-                yield await self._analyze_numeric_column(column_name)
-            elif column_type is DateTimeColumn:
-                yield await self._analyze_datetime_column(column_name)
-            else:
-                yield await self._analyze_string_column(column_name)
+            yield column_name, await self._analyze_column(self._data[column_name])
 
-    def _translate_type(self, column_name: str):
-        type_character = self._data[column_name].dtype.kind
+    async def _analyze_column(self, column: Series):
+        type_character = column.dtype.kind
         if type_character in "iufc":
-            return NumericColumn
+            return await self._analyze_numeric_column(column)
         elif type_character in "M":
-            return DateTimeColumn
+            return await self._analyze_datetime_column(column)
         elif type_character in "m":
-            return NumericColumn
-        elif type_character in "OSU":
-            try:
-                self._data[column_name] = to_datetime(self._data[column_name], errors="raise", format=DATE_TIME_FORMAT)
-                return DateTimeColumn
-            except (ValueError, TypeError):
-                pass
-            try:
-                self._data[column_name] = to_numeric(self._data[column_name], errors="raise")
-                return NumericColumn
-            except (ValueError, TypeError):
-                pass
-        return StringColumn
+            return await self._analyze_numeric_column(column)
 
-    async def _analyze_numeric_column(self, name: str) -> NumericColumn:
-        column = self._data[name]
+        try:
+            datetime_converted = to_datetime(column, errors="raise", format=DATE_TIME_FORMAT)
+            return await self._analyze_datetime_column(datetime_converted)
+        except (ValueError, TypeError):
+            pass
+
+        try:
+            numeric_converted = to_numeric(column, errors="raise")
+            return await self._analyze_numeric_column(numeric_converted)
+        except (ValueError, TypeError):
+            pass
+        return await self._analyze_string_column(column)
+
+    async def _analyze_numeric_column(self, column: Series) -> NumericColumn:
         return NumericColumn(
-            name=name,
+            name=column.name,
             min=numpy_min(column),
             max=numpy_max(column),
             mean=mean(column),
@@ -83,7 +72,7 @@ class Pandas(Analyzer):
             dataType=str(column.dtype),
         )
 
-    async def _analyze_datetime_column(self, name: str) -> DateTimeColumn:
+    async def _analyze_datetime_column(self, column: Series) -> DateTimeColumn:
         CONSISTENCY_INTERVALS = [
             timedelta(seconds=1),
             timedelta(minutes=1),
@@ -92,9 +81,8 @@ class Pandas(Analyzer):
             timedelta(weeks=1),
         ]
 
-        column = self._data[name]
         return DateTimeColumn(
-            name=name,
+            name=column,
             earliest=numpy_min(column),
             latest=numpy_max(column),
             temporalConsistencies=[
@@ -102,8 +90,8 @@ class Pandas(Analyzer):
             ],
         )
 
-    async def _analyze_string_column(self, name: str) -> StringColumn:
-        return StringColumn(name=name)
+    async def _analyze_string_column(self, column: Series) -> StringColumn:
+        return StringColumn(name=column.name)
 
     def _compute_temporal_consistency(self, column: Series, interval: timedelta) -> TemporalConsistency:
         column.index = DatetimeIndex(column)
