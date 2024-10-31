40117c233bdefce003bc8666a2e7e14c4b651e6a
Martin Kaeser <martin.kaeser@exxcellent.de>
2024-10-31 12:24:43 +0100

    Use pathlib instead of os

--- edp/zip.py
+++ edp/zip.py
@@ -1,4 +1,3 @@
-import os
 import zipfile
 from pathlib import Path
 
@@ -9,11 +8,11 @@ def zip_directory(folder_path: Path, output_path: Path):
     # Create a ZipFile object in write mode
     with zipfile.ZipFile(output_path, "w", zipfile.ZIP_DEFLATED) as zipf:
         # Traverse the folder and its subdirectories
-        for root, _, files in os.walk(folder_path):
+        for root, _, files in folder_path.walk():
             for file in files:
                 # Create the full file path
-                file_path = os.path.join(root, file)
+                file_path = root / file
                 # Get the relative path to store in the zip file
-                arcname = os.path.relpath(file_path, folder_path)
+                arcname = file_path.relative_to(folder_path)
                 # Write the file to the zip archive
                 zipf.write(file_path, arcname)
