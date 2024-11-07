b08a1f9a76229d63b99e546274037eaa2da17951
Martin Kaeser <martin.kaeser@exxcellent.de>
2024-11-07 12:23:25 +0100

    Add tests for (un)zipping

---
+++ tests/compression/test_zip.py
@@ -0,0 +1,37 @@
+from zipfile import ZipFile
+
+from pytest import mark, raises
+
+from edp.compression.zip import ZipAlgorithm
+
+
+@mark.asyncio
+async def test_zip_unzip(output_directory):
+    compression_test_dir = output_directory / "compression"
+    src_dir = compression_test_dir / "src"
+    dest_unzipped = compression_test_dir / "unzipped"
+    zip_file = compression_test_dir / "test.zip"
+
+    _create_dummy_files(src_dir)
+
+    zipper = ZipAlgorithm()
+    await zipper.compress(src_dir, zip_file)
+
+    with ZipFile(zip_file, "r") as zipf:
+        zipf.getinfo("test.txt")
+        zipf.getinfo("sub/test2.txt")
+        with raises(KeyError):
+            zipf.getinfo("missingfile.tmp")
+
+    await zipper.extract(zip_file, dest_unzipped)
+
+    assert (dest_unzipped / "test.txt").exists()
+    assert (dest_unzipped / "sub/test2.txt").exists()
+
+
+def _create_dummy_files(dir):
+    (dir / "sub").mkdir(parents=True, exist_ok=True)
+    with open(dir / "test.txt", "wt") as text_io_wrapper:
+        text_io_wrapper.write("Hello world! test.txt")
+    with open(dir / "sub/test2.txt", "wt") as text_io_wrapper:
+        text_io_wrapper.write("Hello world! sub/test2.txt")
