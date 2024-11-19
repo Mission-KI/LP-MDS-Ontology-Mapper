cbeab927997cf72a3e3db384bc081a69569bd252
Kai Buschulte <kb@beebucket.de>
2024-11-19 15:35:11 +0100

    Use detection/sniffer of clevercsv
    Issue: DSE-455

--- edp/importers/pandas.py
+++ edp/importers/pandas.py
@@ -1,7 +1,9 @@
 from asyncio import get_running_loop
-from csv import Sniffer
 from logging import getLogger
 
+from clevercsv import Detector
+from clevercsv.detect import DetectionMethod
+from clevercsv.encoding import get_encoding
 from pandas import read_csv
 
 from edp.analyzers.pandas import Pandas
@@ -12,15 +14,14 @@ async def csv(file: File):
     logger = getLogger("CSV Importer")
     logger.info("Importing %s as CSV", file)
 
-    sniffer = Sniffer()
-    with open(file.path.absolute(), "rt") as open_file:
-        csv_snippet = open_file.read(5000)
-
-    dialect = sniffer.sniff(csv_snippet)
-    has_header = sniffer.has_header(csv_snippet)
+    dialect, has_header = _detect_dialect_and_has_header(file, num_chars=5000)
 
     def runner():
-        return read_csv(file.path.absolute(), sep=dialect.delimiter, header="infer" if has_header else None)
+        return read_csv(
+            file.path,
+            dialect=dialect.to_csv_dialect() if dialect is not None else None,  # type: ignore
+            header="infer" if has_header else None,
+        )
 
     loop = get_running_loop()
     data_frame = await loop.run_in_executor(None, runner)
@@ -32,3 +33,15 @@ async def csv(file: File):
         data_frame.columns = [f"col{i:03d}" for i in range(col_count)]
 
     return Pandas(data_frame, file)
+
+
+def _detect_dialect_and_has_header(file: File, num_chars: int):
+    enc = get_encoding(file.path)
+    # newline="" is important for CSV files as it preserves line endings.
+    # Thus read() passes them through to the detector.
+    with file.path.open("r", newline="", encoding=enc) as fp:
+        data = fp.read(num_chars) if num_chars else fp.read()
+        detector = Detector()
+        dialect = detector.detect(data, verbose=False, method=DetectionMethod.AUTO)
+        has_header = detector.has_header(data)
+    return dialect, has_header
--- tests/importers/test_pandas.py
+++ tests/importers/test_pandas.py
@@ -32,5 +32,49 @@ async def test_import_csv_no_headers():
     assert headers == ["col000", "col001", "col002", "col003", "col004"]
 
 
+async def test_import_csv_with_clevercsv(tmp_path: Path):
+    """Test the advanced detection of clevercsv.
+
+    The standard csv would fail with 'Could not determine delimiter'
+    """
+
+    csv_file = tmp_path / "clever.csv"
+    csv_file.write_text(
+        """Idx;1 col;;3 col;;5 col;
+2022;28 ;16.8 ;49 ;5.7 ;78 ;4.5
+2023;42 ;17.0 ;28 ;5.3 ;61 ;4.3
+"""
+    )
+    file = get_file(csv_file)
+    analyzer = await csv_importer(file)
+    data = analyzer._data
+    row_count = len(data.index)
+    col_count = len(data.columns)
+    headers = data.columns.tolist()
+    assert headers == ["Idx", "1 col", "Unnamed: 2", "3 col", "Unnamed: 4", "5 col", "Unnamed: 6"]
+    assert col_count == 7
+    assert row_count == 2
+    first_row = data.iloc[0]
+    assert first_row.to_list() == [2022.0, 28.0, 16.8, 49.0, 5.7, 78.0, 4.5]
+
+
+async def test_import_csv_with_quoted_strings(tmp_path: Path):
+    csv_file = tmp_path / "clever.csv"
+    csv_file.write_text(
+        """id;value;num
+1;"hello";2023
+2;"world";2024
+"""
+    )
+    file = get_file(csv_file)
+    analyzer = await csv_importer(file)
+    data = analyzer._data
+    assert data.to_dict() == {
+        "id": {0: 1, 1: 2},  # TODO(KB) index needs to be fixed
+        "value": {0: "hello", 1: "world"},
+        "num": {0: 2023, 1: 2024},
+    }
+
+
 def get_file(path: Path):
     return File(path.parent, path)
