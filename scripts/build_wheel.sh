c24e3713cfad147f4238695bda0a3cac07501829
Martin Kaeser <martin.kaeser@exxcellent.de>
2025-01-15 17:21:13 +0100

    Build wheel script should delete all egg-info files

--- scripts/build_wheel.sh
+++ scripts/build_wheel.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 DISTRIBUTION_DIR="./dist"
 
-rm -rf ${DISTRIBUTION_DIR} build *.egg-info **/__pycache__
+rm -rf ${DISTRIBUTION_DIR} build **/*.egg-info **/__pycache__
 mkdir -p ${DISTRIBUTION_DIR}
 echo "${VERSION}" > "${DISTRIBUTION_DIR}/VERSION.txt"
 pip wheel build --no-deps --no-input --no-binary :all: --wheel-dir ${DISTRIBUTION_DIR} .
