0248edae4bf3babca49aaca7c41ebd2148bcd2e8
Joshua Lauterbach <jl@beebucket.de>
2025-03-14 11:16:43 +0100

    Add edps.__version__
    According to PEP 8 __version__ is a magic package property that can be set. This allows for easy introspection
    of the EDPS package version.

--- src/edps/__init__.py
+++ src/edps/__init__.py
@@ -1,3 +1,11 @@
 from extended_dataset_profile.models.v0.edp import ExtendedDatasetProfile as ExtendedDatasetProfile
 
 from edps.service import Service as Service
+from edps.version import __version__
+
+# List of all imports visible when importing this module.
+__all__ = [
+    "ExtendedDatasetProfile",
+    "Service",
+    "__version__",
+]
