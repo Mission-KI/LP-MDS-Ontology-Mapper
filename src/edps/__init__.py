dd8bc382ed5653e8ff3917303b74d489c7656499
Joshua Lauterbach <jl@beebucket.de>
2025-03-21 12:27:21 +0100

    Add Config and UserProvidedEdpData to edp top level
    These are the minimum types needed to invoke the service. Therefore, they should be available in the EDP services top level.

--- src/edps/__init__.py
+++ src/edps/__init__.py
@@ -1,11 +1,6 @@
 from extended_dataset_profile.models.v0.edp import ExtendedDatasetProfile as ExtendedDatasetProfile
 
 from edps.service import Service as Service
-from edps.version import __version__
-
-# List of all imports visible when importing this module.
-__all__ = [
-    "ExtendedDatasetProfile",
-    "Service",
-    "__version__",
-]
+from edps.types import Config as Config
+from edps.types import UserProvidedEdpData as UserProvidedEdpData
+from edps.version import __version__ as __version__
