371f5e7350672ed9ab269136b571c055f340c737
Martin Kaeser <martin.kaeser@exxcellent.de>
2025-03-25 12:58:32 +0100

    Switch to more popular lib "ffmpeg-python" from "python-ffmpeg"

--- src/edps/analyzers/videos/importer.py
+++ src/edps/analyzers/videos/importer.py
@@ -1,11 +1,9 @@
-import json
-import shutil
 from pathlib import Path
 from warnings import warn
 
+import ffmpeg
 import static_ffmpeg
 from extended_dataset_profile.models.v0.edp import Resolution, VideoCodec, VideoDataSet, VideoPixelFormat
-from ffmpeg import FFmpeg
 
 from edps.analyzers.videos import VideoAnalyzer, VideoMetadata
 from edps.taskcontext import TaskContext
@@ -14,20 +12,11 @@ from edps.taskcontext import TaskContext
 async def video_importer(ctx: TaskContext, path: Path) -> VideoDataSet:
     ctx.logger.info("Analyzing video '%s'", ctx.relative_path(path))
 
-    # Blocks until files are downloaded, but only if ffprobe not already on path
-    if not shutil.which("ffprobe"):
-        static_ffmpeg.add_paths()
+    # Blocks until files are downloaded, but only if ffmpeg not already on path
+    static_ffmpeg.add_paths(weak=True)
 
-    probe = FFmpeg(
-        executable="ffprobe",
-    ).input(
-        path,
-        print_format="json",
-        show_streams=None,
-        show_format=None,
-    )
-    media = json.loads(probe.execute())
-    video_streams = [s for s in media.get("streams", []) if s.get("codec_type") == "video"]
+    probe = ffmpeg.probe(path)
+    video_streams = [s for s in probe.get("streams", []) if s.get("codec_type") == "video"]
 
     if not video_streams:
         raise RuntimeError(f'Could not detect video streams for "{ctx.relative_path(path)}"')
@@ -37,7 +26,7 @@ async def video_importer(ctx: TaskContext, path: Path) -> VideoDataSet:
     width = int(video_stream.get("width", 0))
     height = int(video_stream.get("height", 0))
     fps = _compute_fps(ctx, video_stream.get("avg_frame_rate", "0/1"))
-    duration = float(media.get("format", {}).get("duration", "0.0"))
+    duration = float(probe.get("format", {}).get("duration", "0.0"))
     pixel_format = video_stream.get("pix_fmt", "UNKNOWN")
 
     metadata = VideoMetadata(
