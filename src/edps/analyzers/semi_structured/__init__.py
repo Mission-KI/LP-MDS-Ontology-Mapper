17c71701db671100f5e75f4b1fff6847ebd9a8aa
Martin Kaeser <martin.kaeser@exxcellent.de>
2025-04-09 17:44:02 +0200

    Check for valid JSON schema types

--- src/edps/analyzers/semi_structured/__init__.py
+++ src/edps/analyzers/semi_structured/__init__.py
@@ -21,8 +21,10 @@ class JsonAnalyzer:
         builder = SchemaBuilder()
         builder.add_object(self._json_data)
         schema = builder.to_schema()
+        schema_string = json.dumps(schema, indent=2)
+        ctx.logger.debug("Detected JSON schema:\n%s", schema_string)
 
-        matches = await JsonAnalyzer._detect_structured_data(schema)
+        matches = list(_detect_structured_data(schema))
 
         num_dataframes = 0
         async for dataframe in self._extract_dataframes(ctx, matches):
@@ -30,7 +32,7 @@ class JsonAnalyzer:
             await ctx.exec(f"dataframe_{num_dataframes:03}", pandas_importer, dataframe)
 
         return SemiStructuredDataSet(
-            jsonSchema=json.dumps(schema, indent=2),
+            jsonSchema=schema_string,
         )
 
     async def _extract_dataframes(
@@ -38,50 +40,56 @@ class JsonAnalyzer:
     ) -> AsyncIterator[DataFrame]:
         ctx.logger.info("Extracting dataframes...")
         for _, path in matches:
-            structured_data = await JsonAnalyzer._extract_structured_data(self._json_data, path)
+            structured_data = list(_extract_structured_data(self._json_data, path))
             dataframe = json_normalize(list(structured_data))
             yield dataframe
 
-    @staticmethod
-    async def _detect_structured_data(schema: Dict[str, Any]) -> List[StructuredDataMatch]:
-        def traverse(schema_node: Dict[str, Any], path: str):
-            json_type = schema_node.get("type")
-            if json_type == "object":
-                for prop_name, prop_schema in schema_node.get("properties", {}).items():
-                    child_path = f"{path}.{prop_name}" if path else prop_name
-                    yield from traverse(prop_schema, child_path)
-            elif json_type == "array":
-                items_schema = schema_node.get("items", {})
-                if items_schema.get("type") == "object":
-                    yield items_schema, path
-                    yield from traverse(items_schema, path)
 
-        matches = list(traverse(schema, ""))
-        return matches
+def _detect_structured_data(schema_node: Dict[str, Any], path: str = "") -> Iterable[StructuredDataMatch]:
+    json_type = _get_json_type(schema_node)
+    if json_type == "object":
+        for prop_name, prop_schema in schema_node.get("properties", {}).items():
+            child_path = f"{path}.{prop_name}" if path else prop_name
+            yield from _detect_structured_data(prop_schema, child_path)
+    elif json_type == "array":
+        items_schema = schema_node.get("items", {})
+        if _get_json_type(items_schema) == "object":
+            yield items_schema, path
+            yield from _detect_structured_data(items_schema, path)
 
-    @staticmethod
-    async def _extract_structured_data(json_data: JSONData, path: str) -> Iterable[Any]:
-        parts = path.split(".") if path else []
 
-        def extract(curr_data: JSONData, curr_parts: List[str]) -> Iterable[Any]:
-            if not curr_parts:
-                if not (isinstance(curr_data, list) and all(isinstance(item, dict) for item in curr_data)):
-                    raise ValueError(
-                        f"Invalid path: expected a list of dicts (structured data), but got '{type(curr_data).__name__}'."
-                    )
-                yield from curr_data
-            else:
-                if isinstance(curr_data, dict):
-                    key, *remaining_parts = curr_parts
-                    if key not in curr_data:
-                        raise ValueError(f"Invalid path: expected a dict containing key '{key}'.")
-                    yield from extract(curr_data[key], remaining_parts)
-                elif isinstance(curr_data, list):
-                    for item in curr_data:
-                        yield from extract(item, curr_parts)
-                else:
-                    raise ValueError(
-                        f"Invalid path: expected a dict or list when processing path '{path}', but got '{type(curr_data).__name__}'."
-                    )
+def _extract_structured_data(json_data: JSONData, path: str | List[str]) -> Iterable[Any]:
+    if isinstance(path, str):
+        if path:
+            path = path.split(".")
+        else:
+            path = []
 
-        return extract(json_data, parts)
+    if not path:
+        if not (isinstance(json_data, list) and all(isinstance(item, dict) for item in json_data)):
+            raise ValueError(
+                f"Invalid path: expected a list of dicts (structured data), but got '{type(json_data).__name__}'."
+            )
+        yield from json_data
+    else:
+        if isinstance(json_data, dict):
+            key, *remaining_parts = path
+            if key not in json_data:
+                raise ValueError(f"Invalid path: expected a dict containing key '{key}'.")
+            yield from _extract_structured_data(json_data[key], remaining_parts)
+        elif isinstance(json_data, list):
+            for item in json_data:
+                yield from _extract_structured_data(item, path)
+        else:
+            raise ValueError(
+                f"Invalid path: expected a dict or list when processing path '{path}', but got '{type(json_data).__name__}'."
+            )
+
+
+def _get_json_type(schema_node: Dict[str, Any]) -> str:
+    json_type = schema_node.get("type")
+    json_type = json_type.lower().strip() if json_type else ""
+    # Supported types in JSON schema: https://json-schema.org/understanding-json-schema/reference/type
+    if json_type not in ["array", "boolean", "integer", "null", "number", "object", "string"]:
+        raise RuntimeError("Found an invalid type in the JSON schema: %s", json_type)
+    return json_type
